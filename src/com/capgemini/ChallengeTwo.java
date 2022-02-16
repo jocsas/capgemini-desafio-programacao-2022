5b783e3eb66e1f75a186be44df89b32b510e802a
jocsas <mdlimasense@gmail.com>
2022-02-16 00:33:46 -0300

    Add challenge #2

--- src/com/capgemini/ChallengeTwo.java
+++ src/com/capgemini/ChallengeTwo.java
@@ -1,7 +1,31 @@
 package com.capgemini;
 
+import java.util.Scanner;
+
 public class ChallengeTwo {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite sua senha: ");
+
+        String pass = scan.next();
+
+        int passChar = checkPass(pass);
+
+        if(passChar < 6 && passChar != 0){
+            System.out.println("Sua senha precisa ter mais " + passChar + " caracteres.");
+        }else{
+            System.out.println("Senha forte.");
+        }
+
+    }
+    public static int checkPass(String pass) {
+        int minLenght = 6;
+        int passLength = pass.length();
+
+        if (passLength < minLenght){
+            return (minLenght - passLength);
+        }else return 0;
     }
 }
--- src/com/capgemini/Challenges.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini;
-
-public class Challenges {
-    public static void main(String[] args) {
-        System.out.println("teste");
-    }
-}
-
